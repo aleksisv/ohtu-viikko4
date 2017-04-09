0e00831d186f95dc30990982edb6253032facd0c
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-04-09 17:01:47 +0300

    Tehtävä 9 tehty.

--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -14,18 +14,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class Stepdefs {
-    
+
     WebDriver driver = new ChromeDriver();
     String baseUrl = "http://localhost:4567";
-    
-    
+
     @Given("^new user is selected$")
     public void user_selected() throws Throwable {
         driver.get(baseUrl);
         WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
     }
-    
+
     @Given("^login is selected$")
     public void login_selected() throws Throwable {
         driver.get(baseUrl);
@@ -33,21 +32,37 @@ public class Stepdefs {
         element.click();
     }
 
+    @Given("^user with username \"([^\"]*)\" with password \"([^\"]*)\" is successfully created$")
+    public void user_with_username_with_password_is_successfully_created(String arg1, String arg2) throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createUserWith(arg1, arg2);
+    }
+    
+    @Given("^user with username \"([^\"]*)\" and password \"([^\"]*)\" is unsuccessfully created$")
+    public void user_with_username_and_password_is_unsuccessfully_created(String arg1, String arg2) throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createUserWith(arg1, arg2);
+    }
+
     @When("^correct username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_correct_and_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
-    
+
     @When("^correct username \"([^\"]*)\" and incorrect password \"([^\"]*)\" are given$")
     public void username_and_incorrect_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
-    
+
     @When("^nonexistent username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void nonexistent_username_and_password_are_given(String username, String password) throws Throwable {
         logInWith(username, password);
     }
-    
+
     @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_and_password_are_given(String username, String password) throws Throwable {
         WebElement element = driver.findElement(By.name("username"));
@@ -57,7 +72,7 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();
     }
-    
+
     @When("^username \"([^\"]*)\" and password \"([^\"]*)\" and confirmation \"([^\"]*)\" are given$")
     public void username_and_password_and_confirmation_are_given(String arg1, String arg2, String arg3) throws Throwable {
         createUserWith(arg1, arg2);
@@ -67,43 +82,42 @@ public class Stepdefs {
     public void user_is_logged_in() throws Throwable {
         pageHasContent("Ohtu Application main page");
     }
-    
+
     @Then("^user is not logged in and error message is given$")
     public void user_is_not_logged_in_and_error_message_is_given() throws Throwable {
         pageHasContent("invalid username or password");
     }
-    
+
     @Then("^user is registered$")
     public void user_is_registered() throws Throwable {
         pageHasContent("Welcome to Ohtu Application!");
     }
-    
+
     @Then("^user is not created and error \"username should have at least 3 characters\" is reported$")
     public void not_created() throws Throwable {
         pageHasContent("username should have at least 3 characters");
     }
-    
+
     @Then("^user is not created and error \"password should have at least 8 characters\" is reported$")
     public void not_created_password() throws Throwable {
         pageHasContent("password should have at least 8 characters");
     }
-    
+
     @Then("^user is not created and error \"password and password confirmation do not match\" is reported$")
     public void user_is_not_created_password_match() throws Throwable {
         pageHasContent("password and password confirmation do not match");
     }
-    
+
     @Then("^user is not created and error \"password can not contain only letters\" is reported$")
     public void user_is_not_created_and_password_letters() throws Throwable {
         pageHasContent("password can not contain only letters");
     }
-    
+
     @Then("^user is not created and error \"username is already taken\" is reported$")
     public void user_is_not_created_and_username_is_taken_error() throws Throwable {
         pageHasContent("username is already taken");
     }
-    
-    
+
     @After
     public void tearDown() {
         driver.quit();
--- WebLogin/src/test/resources/ohtu/user_creation.feature
+++ WebLogin/src/test/resources/ohtu/user_creation.feature
@@ -1,31 +1,43 @@
 Feature: A new user account can be created if a proper unused username and password are given
+#
+#  Scenario: creation successful with correct username and password
+#    Given new user is selected
+#    When username "uuskayt" and password "salissali2" and confirmation "salissali2" are given
+#    Then user is registered
+#
+#  Scenario: creation fails with too short username and valid passord
+#    Given new user is selected
+#    When username "i" and password "salissali2" and confirmation "salissali2" are given
+#    Then user is not created and error "username should have at least 3 characters" is reported
+#
+#  Scenario: creation fails with correct username and too short password
+#    Given new user is selected
+#    When username "pekkayttaja" and password "p3" and confirmation "p3" are given
+#    Then user is not created and error "password should have at least 8 characters" is reported
+#
+#  Scenario: creation fails with correct username and password consisting of letters
+#    Given new user is selected
+#    When username "uuskayta" and password "kirjainsala" and confirmation "kirjainsala" are given
+#    Then user is not created and error "password can not contain only letters" is reported
+#
+#  Scenario: creation fails with already taken username and valid pasword
+#    Given new user is selected
+#    When username "jukka" and password "hyvasana2" and confirmation "hyvasana2" are given
+#    Then user is not created and error "username is already taken" is reported
+#
+#  Scenario: creation fails when password and password confirmation do not match
+#    Given new user is selected
+#    When username "uuskaytb" and password "salasanajoka2" and confirmation "salasanaj2oka" are given
+#    Then user is not created error "password and password confirmation do not match" is reported
 
-  Scenario: creation successful with correct username and password
-    Given new user is selected
-    When username "uuskayt" and password "salissali2" and confirmation "salissali2" are given
-    Then user is registered
+  Scenario: user can login with successfully generated account
+    Given user with username "liisa" with password "salainen1" is successfully created
+    And login is selected
+    When correct username "liisa" and password "salainen1" are given
+    Then user is logged in
 
-  Scenario: creation fails with too short username and valid passord
-    Given new user is selected
-    When username "i" and password "salissali2" and confirmation "salissali2" are given
-    Then user is not created and error "username should have at least 3 characters" is reported
-
-  Scenario: creation fails with correct username and too short password
-    Given new user is selected
-    When username "pekkayttaja" and password "p3" and confirmation "p3" are given
-    Then user is not created and error "password should have at least 8 characters" is reported
-
-  Scenario: creation fails with correct username and password consisting of letters
-    Given new user is selected
-    When username "uuskayta" and password "kirjainsala" and confirmation "kirjainsala" are given
-    Then user is not created and error "password can not contain only letters" is reported
-
-  Scenario: creation fails with already taken username and valid pasword
-    Given new user is selected
-    When username "jukka" and password "hyvasana2" and confirmation "hyvasana2" are given
-    Then user is not created and error "username is already taken" is reported
-
-  Scenario: creation fails when password and password confirmation do not match
-    Given new user is selected
-    When username "uuskaytb" and password "salasanajoka2" and confirmation "salasanaj2oka" are given
-    Then user is not created error "password and password confirmation do not match" is reported
+  Scenario: user can not login with account that is not successfully created
+    Given user with username "aa" and password "bad" is unsuccessfully created
+    And login is selected
+    When nonexistent username "aa" and password "bad" are given
+    Then user is not logged in and error message is given
